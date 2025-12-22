f55830a9d3ef7593fd6c969f23888fd98fca2225
Saeed-Altout <saeedaltoutpro@gmail.com>
2025-12-22 14:39:24 +0300

    feat: Add Marquee UI component and integrate it for testimonials on the marketing home page.

--- app/(marketing)/(routes)/(home)/page.tsx
+++ app/(marketing)/(routes)/(home)/page.tsx
@@ -1,9 +1,11 @@
 import { Hero } from "./_components/hero";
+import { Testimonials } from "./_components/testimonials";
 
 export default function MarketingPage() {
   return (
     <main className="flex-1">
       <Hero />
+      <Testimonials />
     </main>
   );
 }
